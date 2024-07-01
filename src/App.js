4f26ac06206441a0a8c508777ec216c7f77ed74f
4thdraw <4thdraw2@gmail.com>
2024-07-01 13:21:40 +0900

    폰트적용

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+        <p className='noto400'>
+          글자변경 <span className='noto100'>적용되었나?</span>
+          <span className='noto500'>적용되었나?</span>
+          <span className='noto600'>적용되었나?</span>
+          <span className='noto800'>적용되었나?</span>
+          <span className='noto900'>적용되었나?</span>
+        </p> 
+  );
+}
+
+export default App;
